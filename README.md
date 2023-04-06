82e4a496389b20733330908e9d3a5f6e39e63798
jparrker <102701917+jparrker@users.noreply.github.com>
2023-04-06 18:45:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple react app for processing a list of objects and rendering them as cards.Demonstration of useState for loading and data and useEffect for fetching data
---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Tours from "./Tours";
+import Loading from "./Loading";
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id != id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={fetchTours}
+          >
+            Refetch tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+export default App;
